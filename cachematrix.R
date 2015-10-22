dc63a4a21618dacaf6c661dccbc9c8124cccf38a
Kenneth Platz <kplatz@utdallas.edu>
2015-10-22 08:02:56 -0500

    Testing

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ci <- NULL
+    set <- function(y) {
+        x <<- y
+        ci <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) ci <<- inverse
+    getinverse <- function() ci
+    list( set = set, get = get, setinverse = setinverse, getinverse = getinverse );
 }
 
 
 ## Write a short comment describing this function
 
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+    ## Attempt to retrieve the cached inverse matrix
+    cm <- x$getinverse()
+
+    ## if the result is non-NULL, return it
+    if ( !is.null(cm) ) {
+        message("Getting cached data")
+        return(cm)
+    }
+
+    ## If we've made it this far, the value has NOT been cached.
+    data <- x$get()         ## Get the stored data
+    cm <- solve(data,...)   ## Calculate the inverse
+    x$store(cm)             ## Store the cached mean
+    cm                      ## And return it.
 }
